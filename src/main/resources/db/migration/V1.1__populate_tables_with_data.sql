57da943ee62cff8dce1841b176c78a9dfd4960f1
Guilherme <guilhermeotta@gmail.com>
2020-02-09 12:08:59 -0300

    Ajusta dados salvos por padrão na base de dados.

--- src/main/resources/db/migration/V1.1__populate_tables_with_data.sql
+++ src/main/resources/db/migration/V1.1__populate_tables_with_data.sql
@@ -3,11 +3,13 @@
 -- ---
 
 -- INSERT INTO `rule` (`id`,`initial_day`,`final_day`) VALUES ('','','');
-INSERT INTO `rule` (`id`, `initial_day`, `final_day`) VALUES(1, 1, 3);
-INSERT INTO `rule` (`id`, `initial_day`, `final_day`) VALUES(2, 4, 5);
-INSERT INTO `rule` (`id`, `initial_day`, `final_day`) VALUES(3, 6, 9223372036854775807);
+INSERT INTO `rule` (`id`, `initial_day`, `final_day`) VALUES(1, -9223372036854775807, 0);
+INSERT INTO `rule` (`id`, `initial_day`, `final_day`) VALUES(2, 1, 3);
+INSERT INTO `rule` (`id`, `initial_day`, `final_day`) VALUES(3, 4, 5);
+INSERT INTO `rule` (`id`, `initial_day`, `final_day`) VALUES(4, 6, 9223372036854775807);
 -- INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES('','','','');
-INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES(1, 2, 0.1, 1);
-INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES(2, 3, 0.2, 2);
-INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES(3, 5, 0.3, 3);
+INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES(1, 0, 0, 1);
+INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES(2, 2, 0.1, 2);
+INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES(3, 3, 0.2, 3);
+INSERT INTO `fine` (`id`,`percentage`,`interest`,`id_rule`) VALUES(4, 5, 0.3, 4);
 -- INSERT INTO `bill` (`id`,`name`,`value`,`due_date`,`payday`,`late_days`,`id_fine`) VALUES('','','','','','','');
